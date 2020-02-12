23a767332facbf30c78b29489dc1c42aa085fe62
[InnCreator] <[engbethany4@gmail.com]>
2020-02-12 13:14:23 +0300

    will update

---
+++ Personal Portfolio/WebComponent.js
@@ -0,0 +1,277 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const WebComponent = () => (
+  <svg width={1920} height={1080} viewBox="0 0 1920 1080">
+    <defs>
+      <style>
+        {
+          ".a{clip-path:url(#o);}.b{fill:none;stroke:#010101;stroke-linecap:round;stroke-width:9px;}.c,.d,.e,.i{font-size:54px;}.c,.d,.i{font-family:Helvetica;}.d{fill:#0d0d0d;}.e,.i{fill:#6359a6;}.e{font-family:Helvetica-Bold, Helvetica;font-weight:700;}.f{fill:url(#a);}.g{fill:#fff;}.h{fill:url(#d);}.j{fill:url(#e);}.k{fill:url(#f);}.l{fill:url(#j);}.m{fill:url(#l);}.n{filter:url(#h);}.o{filter:url(#b);}"
+        }
+      </style>
+      <pattern
+        id="a"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 686 311"
+      >
+        <image
+          width={686}
+          height={311}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CDownloads%5Cfauydegw.png"
+        />
+      </pattern>
+      <filter
+        id="b"
+        x={584}
+        y={181}
+        width={1178}
+        height={297}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="c" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="c" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <pattern
+        id="d"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 810"
+      >
+        <image
+          width={1080}
+          height={810}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CAppData%5CLocal%5CPackages%5CAdobe.CC.XD_adky2gkssdxte%5CTempState%5CUXP%5CPluginsTemp%5CExternal%5Cb047cc28%5Ctmp"
+        />
+      </pattern>
+      <pattern
+        id="e"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 1350"
+      >
+        <image
+          width={1080}
+          height={1350}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CAppData%5CLocal%5CPackages%5CAdobe.CC.XD_adky2gkssdxte%5CTempState%5CUXP%5CPluginsTemp%5CExternal%5Cb047cc28%5Ctmp"
+        />
+      </pattern>
+      <pattern
+        id="f"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 720"
+      >
+        <image
+          width={1080}
+          height={720}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CAppData%5CLocal%5CPackages%5CAdobe.CC.XD_adky2gkssdxte%5CTempState%5CUXP%5CPluginsTemp%5CExternal%5Cb047cc28%5Ctmp"
+        />
+      </pattern>
+      <filter
+        id="h"
+        x={584}
+        y={649}
+        width={1178}
+        height={301}
+        filterUnits="userSpaceOnUse"
+      >
+        <feOffset dy={3} input="SourceAlpha" />
+        <feGaussianBlur stdDeviation={3} result="i" />
+        <feFlood floodOpacity={0.161} />
+        <feComposite operator="in" in2="i" />
+        <feComposite in="SourceGraphic" />
+      </filter>
+      <pattern
+        id="j"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 780"
+      >
+        <image
+          width={1080}
+          height={780}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CAppData%5CLocal%5CPackages%5CAdobe.CC.XD_adky2gkssdxte%5CTempState%5CUXP%5CPluginsTemp%5CExternal%5Cb047cc28%5Ctmp"
+        />
+      </pattern>
+      <pattern
+        id="l"
+        preserveAspectRatio="xMidYMid slice"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1080 1434"
+      >
+        <image
+          width={1080}
+          height={1434}
+          xlinkHref="C:%5CUsers%5CInnCreator%5CAppData%5CLocal%5CPackages%5CAdobe.CC.XD_adky2gkssdxte%5CTempState%5CUXP%5CPluginsTemp%5CExternal%5Cb047cc28%5Ctmp"
+        />
+      </pattern>
+      <clipPath id="o">
+        <rect width={1920} height={1080} />
+      </clipPath>
+    </defs>
+    <g id="n" className="a">
+      <rect className="g" width={1920} height={1080} />
+      <line className="b" y2={934} transform="translate(476.5 73.5)" />
+      <text className="c" transform="translate(76 314)">
+        <tspan x={0} y={51}>
+          {"About"}
+        </tspan>
+      </text>
+      <text className="d" transform="translate(76 444)">
+        <tspan x={0} y={51}>
+          {"Blog"}
+        </tspan>
+      </text>
+      <text className="e" transform="translate(76 574)">
+        <tspan x={0} y={52}>
+          {"Portfolio"}
+        </tspan>
+      </text>
+      <text className="c" transform="translate(76 704)">
+        <tspan x={0} y={51}>
+          {"Contact"}
+        </tspan>
+      </text>
+      <rect
+        className="f"
+        width={322}
+        height={141}
+        transform="translate(76 74)"
+      />
+      <g className="o" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <rect
+          className="g"
+          width={1160}
+          height={279}
+          transform="translate(593 187)"
+        />
+      </g>
+      <g transform="translate(0 42)">
+        <rect
+          className="h"
+          width={264}
+          height={175}
+          transform="translate(593 145)"
+        />
+        <text className="i" transform="translate(609 349)">
+          <tspan x={0} y={51}>
+            {"Posters"}
+          </tspan>
+        </text>
+      </g>
+      <rect
+        className="j"
+        width={264}
+        height={175}
+        transform="translate(893 187)"
+      />
+      <text className="i" transform="translate(909 391)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <rect
+        className="k"
+        width={264}
+        height={175}
+        transform="translate(1193 187)"
+      />
+      <text className="i" transform="translate(1209 391)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <rect
+        className="k"
+        width={264}
+        height={175}
+        transform="translate(1493 187)"
+      />
+      <text className="i" transform="translate(1509 391)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <g className="n" transform="matrix(1, 0, 0, 1, 0, 0)">
+        <rect
+          className="g"
+          width={1160}
+          height={283}
+          transform="translate(593 655)"
+        />
+      </g>
+      <rect
+        className="l"
+        width={264}
+        height={175}
+        transform="translate(593 655)"
+      />
+      <text className="i" transform="translate(609 859)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <rect
+        className="k"
+        width={264}
+        height={175}
+        transform="translate(893 655)"
+      />
+      <text className="i" transform="translate(909 859)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <rect
+        className="m"
+        width={264}
+        height={175}
+        transform="translate(1193 655)"
+      />
+      <text className="i" transform="translate(1209 859)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <rect
+        className="k"
+        width={264}
+        height={175}
+        transform="translate(1493 655)"
+      />
+      <text className="i" transform="translate(1509 859)">
+        <tspan x={0} y={51}>
+          {"Posters"}
+        </tspan>
+      </text>
+      <text className="i" transform="translate(593 91)">
+        <tspan x={0} y={51}>
+          {"Graphic and Web Design"}
+        </tspan>
+      </text>
+      <text className="i" transform="translate(593 540)">
+        <tspan x={0} y={51}>
+          {"Coded websites and Vizualizations"}
+        </tspan>
+      </text>
+    </g>
+  </svg>
+);
+
+export default WebComponent;
